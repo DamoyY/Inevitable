80e323e45297f794d30a6191a9fa68ad2465f1e4
Damoy <Damoya@proton.me>
2026-01-09 08:12:35 +0800

    移除单线程模式

--- src/pns/mod.rs
+++ src/pns/mod.rs
@@ -1,11 +1,7 @@
 pub mod node;
 pub mod parallel;
-pub mod search;
-pub mod solver;
-pub mod tree;
 pub mod tt;
 
 pub use node::PNSNode;
 pub use parallel::ParallelSolver;
-pub use solver::PNSSolver;
 pub use tt::TTEntry;
